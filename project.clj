779ad01d9613b0c7a0ced548a95ab6c7aef35008
Aaron Lifton <aaronlifton@gmail.com>
2018-12-30 00:12:03 -0800

    update project.clj

--- project.clj
+++ project.clj
@@ -1,17 +1,14 @@
 (defproject lilrouter "0.1.0-SNAPSHOT"
   :description "A simple clojure HTTP router."
-  :url "http://github.com/aaronlifton2/lilrouter"
+  :url "https://github.com/aaronlifton2/lilrouter"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                   [ring/ring-core "1.6.3"]
                   [ring/ring-jetty-adapter "1.6.3"]
                   [ring/ring-devel "1.6.3"]
-                  [ragtime "0.8.0"]
-                  [org.xerial/sqlite-jdbc "3.7.2"]
                   [org.clojure/tools.logging "0.4.1"]]
   :plugins [[lein-ring "0.12.1"]]
-  :main lilrouter.core
   :repl-options {:init-ns lilrouter.core
                  :init (use 'lilrouter.repl-helpers)}
   :ring {:handler demo.index/handler}
